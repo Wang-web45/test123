a2f361f889e9825bc8552d9984c3f35ddd81d0ce
wgxedit <wgxedit@gmail.com>
2021-07-14 21:23:15 +0800

    李四

--- src/Student.java
+++ src/Student.java
@@ -6,4 +6,10 @@ public class Student {
     private Integer age;
     private String name;
     private Integer stress;
+
+    public Student(Integer age, String name, Integer stress) {
+        this.age = age;
+        this.name = name;
+        this.stress = stress;
+    }
 }
